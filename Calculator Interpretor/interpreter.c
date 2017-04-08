2f0d4402c924778e8514380b8af4f69bd2382b8c
sTux <esyray15@gmail.com>
2017-04-08 12:59:22 +0530

    FInishing up interpreter

--- Calculator Interpretor/interpreter.c
+++ Calculator Interpretor/interpreter.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <malloc.h>
 
-#define PLUS 1
-#define EOF EOF
+#define PLUS '+'
+#define EOF 'EOF'
+#define INTEGER 'INTEGER'
+
 
 typedef struct Token {
-    int type;
+    char *type;
     double float value;
 } token_t;
 
@@ -17,8 +21,9 @@ typedef struct Interpreter {
 } interpreter_t;
 
 
-void token_init(token_t *token, type, value) {
-    token->type = type;
+void token_init(token_t *token, char *type, double float value) {
+    token->type = (char *)calloc(sizeof(char), strlen(type));
+    strcpy(token->type, type);
     token->value = value;
 }
 
@@ -29,5 +34,28 @@ char *token_repr(token_t *token) {
 
 
 void interpreter_init(interpreter_t *inter, char *text) {
-    
+    inter->text = (char *)calloc(sizeof(char), strlen(text));
+    strcpy(inter->text, text);
+    inter->pos = 0;
+    inter->current_token = NULL;
+}
+
+
+char *interpreter_nextToken(interpreter_t *inter) {
+    double float digit[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char *text = (char *)calloc(sizeof(char), strlen(inter->text));
+
+    if(inter->pos > strlen(inter->text)-1) {
+        token_t *eof = (token_t *)malloc(sizeof(token_t));
+        token_init(eof, EOF, NULL);
+        return eof;
+    }
+
+    char current_char = text[inter->pos];
+    if(isdigit(current_char)) {
+        token_t *token = (token_t *)malloc(sizeof(token_t));
+        token_init(token, INTEGER, digit[current_char]);
+        token->pos += 1;
+        return token;
+    }
 }
