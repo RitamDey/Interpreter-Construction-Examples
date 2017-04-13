02a9b51cca402eb0cf6cc9c5cc90e45cff9003a9
sTux <esyray15@gmail.com>
2017-04-13 22:35:07 +0530

    Cleanup magic Signed-off-by: sTux <esyray15@gmail.com>

--- Let's Build A Simple Interpreter/Calculator Interpretor/calculator.py
+++ Let's Build A Simple Interpreter/Calculator Interpretor/calculator.py
@@ -4,6 +4,13 @@ from collections import deque
 
 
 INTEGER, OPERATOR, EOF = 'INTEGER', 'OPERATOR', 'EOF'
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+    '^': operator.pow,
+}  # Opeartor dictionary. Copied from a StackOverflow post
 
 
 class Token:
@@ -38,33 +45,34 @@ class Interpreter:
         self.values = deque()
 
     def gen_token(self):
+        """
+        Tokenizar for the interpreter. Converts a character to token one at a time
+        """
         try:
+            # Try to get the current character from list
             char = self.text[self.pos]
-        except:
+        except IndexError:
+            # self.pos is pointing to a value out of the index of the list and
+            # that means we have tokenized the entire list
             return Token(EOF, None)
 
-        self.pos += 1
+        self.pos += 1  # Points to the next character in list to tokenize
 
         try:
+            # Try to convert the char into a float, if it can be coverted
             return Token(INTEGER, float(char))
         except ValueError:
-            if char == '+':
-                return Token(OPERATOR, lambda x, y: x+y)
-            elif char == '-':
-                return Token(OPERATOR, lambda x, y: x-y)
-            elif char == '*':
-                return Token(OPERATOR, lambda x, y: x*y)
-            elif char == '/':
-                return Token(OPERATOR, lambda x, y: x/y)
-            elif char == '^':
-                return Token(OPERATOR, lambda x, y: x**y)
+            # If we are here then it surely means that this character is not a number
+            if char in OPERATORS.keys():
+                return Token(OPERATOR, OPERATORS[char])
             else:
+                # For sanity check
                 raise ValueError(f'Unknown operator {char}.')
 
     def eval(self, statment):
-        # Creates a list of the values and the opertion passed
+        # Creates a list of the values and the opertion passed, its easier to evaluate
         self.text = findall(r'[\.0-9]+|[\+\-\*\/\^]+', statment)
-        result = None
+        result = None  # Just a placeholder for result in method scope
 
         while True:
             token = self.gen_token()
